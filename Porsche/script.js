67582e834bf45f0d86f3edecd456fae431cdefb1
Lityem <lityempro@gmail.com>
2024-08-21 19:06:15 +0300

    Add files via upload

---
+++ Porsche/script.js
@@ -0,0 +1,20 @@
+function PlaySound() {
+    audio = new Audio("start.mp3");
+    audio.play();
+  }
+  function Loadsite() {
+    loadingScreen = document.getElementById("loading");
+  
+    loadingScreen.classList.remove("loading");
+    loadingScreen ].classist.remove("loadingHide")
+    loadingScreen.classist.add("loadingDelete");
+  }
+  function loadSite()
+  {
+      loadingScreen = document.getElementById("loading")
+  
+      loadingScreen.classList.add("loadingHide")
+      setTimeout(hide, 1400)
+      setTimeout(playSound, 800);
+  }
+  
